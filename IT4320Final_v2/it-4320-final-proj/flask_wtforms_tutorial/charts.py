7120dbe500fef85ae7af060bf309899d352cbf39
JShanika <70115333+JShanika@users.noreply.github.com>
2021-05-04 21:15:33 -0500

    get_total_cost function

--- IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/charts.py
+++ IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/charts.py
@@ -26,3 +26,14 @@ def print_flightChart(flight):
         map+="\n"
         
     return map
+
+def get_total_sales():
+    file = open("reservations.txt")
+    total_sales = 0
+    cost_matrix = get_cost_matrix()
+    for data in file:
+        reserved = data.split(",")
+        row = int(reserved[1].strip())
+        column = int(reserved[2].strip())
+        total_sales += cost_matrix[row][col]
+    return total_sales
