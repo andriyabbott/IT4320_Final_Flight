d5648eb0cfda4df10a0189b848dbbc0d9c09da37
Wei Syuan, Li <47014256+syuan2000@users.noreply.github.com>
2021-05-05 23:08:54 +0800

    Add files via upload

--- IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/charts.py
+++ IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/charts.py
@@ -26,3 +26,62 @@ def print_flightChart(flight):
         map+="\n"
         
     return map
+
+
+def ticket_number(first):
+    className = "INFOTC4320"
+    if len(className)>len(first):
+        bookingID = "".join([first[i] + className[i] for i in range(len(first))]) + className[len(first):]
+    elif len(className)<len(first):
+        bookingID = "".join([first[i] + className[i] for i in range(len(first))]) + first[len(className):]
+    else:
+        bookingID = "".join([first[i] + className[i] for i in range(len(first))])
+    return bookingID
+
+def assign_seat(flight, row, seat):
+    row=int(row)
+    seat=int(seat)
+    if flight[row-1][seat-1] == "O":
+        flight[row-1][seat-1] = "X"
+        
+        return True, flight
+    else:
+        print("\nRow:{} Seat:{} is already assigned. Choose again.\n".format(row, seat))
+        return False, flight
+
+def saveToText(first_name, row, seat, bookingID):
+    row=int(row)
+    seat=int(seat)
+    info=first_name+', '+str(row-1)+', '+str(seat-1)+', '+bookingID+'\n'
+    try:
+        save_data = open('flask_wtforms_tutorial/reservations.txt', 'a')
+        save_data.write(info)
+    except:
+        print("Error updating the reservation to .txt file")
+
+    save_data.close()
+    return
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
+
+def sales(flight, cost_matrix):
+    total= 0
+
+    for r in range(len(flight)):
+        for c in range(len(flight[r])):
+            if flight[r][c] == "X":
+                total+= cost_matrix[r][c]
+    
+    return total
+def admin():
+    admins=[]
+    admin={}
+    file=open("flask_wtforms_tutorial/passcodes.txt", "r")
+    for line in file:
+        login = line.split(",")
+        
+        admin["username"]=login[0]
+        admin["password"]=login[1].strip()
+        admins.append(admin)
+    return admins
--- IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/routes.py
+++ IT4320Final_v2/it-4320-final-proj/flask_wtforms_tutorial/routes.py
@@ -7,6 +7,7 @@ from .charts import *
 seat = emptySeats(12, 4)
 flight = initialSeats(seat)
 
+
 #@app.route("/", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def user_options():
@@ -24,25 +25,28 @@ def user_options():
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
-
+    message=None
     form = AdminLoginForm()
 
     if request.method == 'POST' and form.validate_on_submit():
         #make sure the admin username and password are correct...
         username = request.form["username"]
         password = request.form["password"]
+        cost=get_cost_matrix()
+        sale=sales(flight, cost)
 
-
-        for line in open("flask_wtforms_tutorial/passcodes.txt", "r").readlines():
-            login = line.split(",")
+        file=open("flask_wtforms_tutorial/passcodes.txt", "r")
+        for line in file:
+            login=line.split(", ")
             if username == login[0] and password == login[1].strip():
                 err= None
                 chart = print_flightChart(flight)
-                return render_template("admin.html", form=form,template="form-template", err=err, chart = chart)
-            else:
-                err = "You've enterned the wrong information"
-                chart = None
-                return render_template("admin.html", form=form,template="form-template", err=err, chart = chart) 
+                message= "Total sales: ${}".format(sale)
+                return render_template("admin.html", form=form,template="form-template", err=err, chart = chart, message=message)
+
+        err = "Invalid username or password. Try again. "
+        chart = None
+        return render_template("admin.html", form=form,template="form-template", err=err, chart = chart) 
                 
 
     return render_template("admin.html", form=form, template="form-template")
@@ -50,7 +54,32 @@ def admin():
 @app.route("/reservations", methods=['GET', 'POST'])
 def reservations():
 
+    chart = print_flightChart(flight)
+    err=None
+    message=None
+
     form = ReservationForm()
 
-    return render_template("reservations.html", form=form, template="form-template")
+    if request.method == 'POST' or form.validate_on_submit():
+        firstName = request.form["first_name"]
+        lastName = request.form["last_name"]
+        seat = request.form["seat"]
+        row = request.form["row"]
+        success,updateFlight=assign_seat(flight,row,seat)
+        bookingID=ticket_number(firstName)
+        
+    
+        if success:
+            
+            chart = print_flightChart(updateFlight)
+            message = "\nCongradulation {}! Seat Row {} Seat {} has been assigned.\n".format(firstName,row, seat)
+            message+= "\nYour eticket number is : " + bookingID
+            saveToText(firstName, row, seat, bookingID)
+            return render_template("reservations.html", form=form, template="form-template", err=err, chart = chart, message=message)
+        else:
+            err= "Error: Row {} Seat {} has been assigned!\n".format(row,seat)
+            return render_template("reservations.html", form=form, template="form-template", err=err, chart = chart)
+
+    return render_template("reservations.html", form=form, template="form-template",chart=chart)
+
 
